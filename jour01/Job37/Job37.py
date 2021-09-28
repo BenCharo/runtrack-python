7cfcf7fb6bc0f16a30f0298f6bd01d935f8e8f84
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-09-28 23:39:37 +0200

    MAJ Job37

--- jour01/Job37/Job37.py
+++ jour01/Job37/Job37.py
@@ -1,14 +1,31 @@
-def plus_proche(chain):
-    longueur = len(chain)
-    for i in range(1, longueur):
-        k = chain[longueur - i]
-        j = i+1
-        while k < chain[longueur - j]:
-            j = j + 1
-        
+def plus_proche(text):
+    text_ascii = []
+    long = len(text) - 1
+    for i in text:
+        text_ascii.append(ord(i)) #changement en code ascii
+    print(text_ascii)
+    cpt = 1
+    j = long
+    while text_ascii[j] < text_ascii[j - cpt]:
+        cpt += 1
+        if cpt > j:
+            cpt = 1
+            j -= 1
+
+    text_ascii[j], text_ascii[j-cpt] = text_ascii[j-cpt], text_ascii[j]
+    print(text_ascii)
+
+    for k in range(j-cpt + 1, long):
+        if text_ascii[k] > text_ascii[k+1]:
+            text_ascii[k], text_ascii[k+1] = text_ascii[k+1], text_ascii[k]
+    print(text_ascii)
+
+    next_word = []
+    for l in text_ascii:
+        next_word.append(chr(l)) #changement en texte
+
+    return next_word
 
 
-
-
-x = plus_proche("abcde")
+x = plus_proche("cdba")
 print(x)
