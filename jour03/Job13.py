359800937b6b8bf7ae1a0b69fc500550f5804b77
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-09-30 17:04:33 +0200

    Jour 03, debut job21

--- jour03/Job13.py
+++ jour03/Job13.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import re
 
 file = open("data.txt", "r")
 line = file.read()
---
+++ jour03/Job21.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import re
+
+file = open("data.txt", "r")
+line = file.read()
+file.close()
+line.lower()
+x = "abcdefghijklmnopqrstuvwxyz"
+alph = sorted(x)
+
+split = line.split()
+
+tab = []
+
