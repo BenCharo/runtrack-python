ee3956b942176c10667bb50f743b0d025384d1b9
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-09-30 13:45:53 +0200

    Jour 03, Job02

--- jour03/Job02.py
+++ jour03/Job02.py
@@ -3,14 +3,15 @@ import re
 file = open("data.txt", "r")
 line = file.read()
 file.close()
-x = re.split("\s", line)
+x = re.split("\s", line) #on sépare le texte en tableau de mot pour les tester un a un
 cpt = 0
 
 
 
 for i in x:
+    if i[-1:] == "." or i[-1:] == "," or i[-1:] == ";" or i[-1:] == ":": #on vérifie que les caracteres speciaux des mots ne sont
+        i = i[:-1]                                                       #pas dûs à la ponctuation, et on les supprime dans ce cas la
     if i.isalnum() is True:
-        cpt += 1
+        cpt += 1            #on incrémente le compteur dès qu'on est tombé sur un mot sans caractères spéciaux
 
-print(x)
-#print("Il y a", cpt, "mots sans caractères spéciaux dans data.txt")
+print("Il y a", cpt, "mots sans caractères spéciaux dans data.txt")
