8c6de4ed9a39c0532e1c7898596485b2e781b464
Benjamin Bigot <91526806+BenCharo@users.noreply.github.com>
2021-10-01 16:49:23 +0200

    Jour 04. job08 (non opérationnel)

--- jour04/Job08.py
+++ jour04/Job08.py
@@ -3,10 +3,9 @@ def dame(x, y, nbDames):
         return
     else:
         if x > n:
-             x = x - n
+            x = x - n
         board[x][y] = "X"
         dame(x + 2, y + 1, nbDames - 1)
-
 
 
 n = int(input("Taille du côté :\n"))
@@ -14,7 +13,7 @@ n = int(input("Taille du côté :\n"))
 board = [["O"] * n for i in range(n)]
 
 if n % 3 == 0 and n % 2 == 0:
-     dame(1, 0, n)
+    dame(0, 1, n)
 elif n % 2 != 0:
     dame(0, 0, n)
 else:
